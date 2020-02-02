9ff8545297f5ec2811c5bdb8c5206c2017f68f60
frekdote <frekdote@gmail.com>
2020-02-02 17:17:49 +0900

    Modify check.sh

--- check.sh
+++ check.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env sh
 
-env
-echo "----------"
-ls -l
-echo "----------"
-ls -l /
-
-# coq_makefile -f _CoqProject *.v -o Makefile
-# make
-# r=$?
-# make clean
-# exit $r
+coq_makefile -f _CoqProject *.v -o Makefile
+make
+r=$?
+make clean
+exit $r
